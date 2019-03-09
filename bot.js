c124df290f4eb0420ad487da679ff732973616a9
Ethan Tilley <1008464@student.sae.edu.au>
2019-03-09 18:42:33 +1000

    Tested out the get() and post function. Looked at some tweets and posted the first one as a test!

--- bot.js
+++ bot.js
@@ -4,3 +4,31 @@ var Twit = require( 'twit' );
 
 var config = require( './config' );
 var T = new Twit( config );
+
+var params = {
+  q: 'indiedev',
+  count: 2
+}
+
+var newTweet = {
+  status: '#myfirstTweet From a bot using node.js!'
+}
+
+//T.get( 'search/tweets', params, retriveData );
+T.post( 'statuses/update', newTweet, retriveData );
+
+function retriveData( err, data, responce ) {
+  // var tweets = data.statuses;
+  // for ( var i = 0; i < tweets.length; i++ ) {
+  //   console.log( tweets[ i ].text );
+  //
+  // }
+
+  if ( !err ) {
+    console.log( "Tweeted: " + data.statuses );
+  } else {
+    console.log( "Tweet failed" );
+
+  }
+
+}
